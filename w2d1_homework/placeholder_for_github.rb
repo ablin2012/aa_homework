409ab69ed350be042196b5150cd015e4e7c69301
ablin2012 <adamlin.2009@gmail.com>
2022-05-03 21:39:27 -0700

    w3d3 hw

---
+++ w2d1_homework/placeholder_for_github.rb
@@ -0,0 +1,11 @@
+puts "Will you marry me? yes or no"
+
+answer = gets.chomp
+
+if answer == "yes"
+  p "You've made me the happiest guy in the world"
+elsif answer == "no"
+  p "FUCK YOU, YOU DIRTY WHORE. I HOPE YOU ROT IN HELL WITH YOU SMELLY ARMPITS AND YOUR LONG ASS NOSE HAIRS. I DIDN'T EVEN WANT YOU ANYWAY. YOU'RE LIKE A 4 AT BEST."
+else
+  p "bro it's a yes or no question"
+end
